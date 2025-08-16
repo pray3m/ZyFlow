2238b0d65b5a65dbe999537a752eed265b6eacc8
pray3m <prem.gtm9@gmail.com>
2025-08-16 14:48:42 +0545

    feat: add WorkflowExecution and ExecutionPhase models; implement workflow execution

--- actions/workflows/runWorkflow.ts
+++ actions/workflows/runWorkflow.ts
@@ -1,9 +1,17 @@
 "use server";
 
 import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { number } from "zod";
 import prisma from "@/lib/prisma";
 import { FlowToExecutionPlan } from "@/lib/workflow/executionPlan";
-import type { WorkflowExecutionPlan } from "@/types/workflow";
+import { TaskRegistry } from "@/lib/workflow/task/registry";
+import {
+  ExecutionPhaseStatus,
+  type WorkflowExecutionPlan,
+  WorkflowExecutionStatus,
+  WorkflowExecutionTrigger,
+} from "@/types/workflow";
 
 export async function RunWorkflow(form: {
   workflowId: string;
@@ -46,5 +54,37 @@ export async function RunWorkflow(form: {
   }
 
   executionPlan = result.executionPlan;
-  console.log("@Execution Plan", executionPlan);
+
+  const execution = await prisma.workflowExecution.create({
+    data: {
+      workflowId,
+      userId,
+      status: WorkflowExecutionStatus.PENDING,
+      startedAt: new Date(),
+      trigger: WorkflowExecutionTrigger.MANUAL,
+      phases: {
+        create: executionPlan.flatMap((phase) => {
+          return phase.nodes.flatMap((node) => {
+            return {
+              userId,
+              status: ExecutionPhaseStatus.CREATED,
+              number: phase.phase,
+              node: JSON.stringify(node),
+              name: TaskRegistry[node.data.type].label,
+            };
+          });
+        }),
+      },
+    },
+    select: {
+      id: true,
+      phases: true,
+    },
+  });
+
+  if (!execution) {
+    throw new Error("workflow execution not created");
+  }
+
+  redirect(`/workflow/runs/${workflowId}/${execution.id}`);
 }
